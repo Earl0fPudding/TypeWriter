37b5caaed00d1797aafbfc7c0aabc5f47d72de5f
Earl0fPudding <martin@weidenauer.cc>
2020-02-01 21:23:10 +0100

    Add search forms

--- webapp/forms.py
+++ webapp/forms.py
@@ -7,3 +7,21 @@ class CommentForm(forms.Form):
     text = forms.CharField(widget=forms.Textarea, required=True)
     content_id = forms.IntegerField(required=True)
     answer_to = forms.IntegerField(required=False)
+
+
+class DiscoverForm(forms.Form):
+    keywords = forms.CharField(max_length=200, required=False),
+    title_or_both = forms.RadioSelect(choices={"title", "both"}),
+    languages = forms.CheckboxSelectMultiple(),
+    all_cat = forms.RadioSelect(choices={"all", "select"}),
+    categories = forms.CheckboxSelectMultiple(),
+    start_day = forms.IntegerField(required=True, min_value=1, max_value=31),
+    start_month = forms.IntegerField(required=True, min_value=1, max_value=12),
+    start_year = forms.IntegerField(required=True, min_value=2020, max_value=3000),
+    end_day = forms.IntegerField(required=True, min_value=1, max_value=31),
+    send_month = forms.IntegerField(required=True, min_value=1, max_value=12),
+    end_year = forms.IntegerField(required=True, min_value=2020, max_value=3000)
+
+
+class SearchForm(forms.Form):
+    keywords = forms.CharField(max_length=200, min_length=1, required=True)
