e85b163b07cee50c3b1f71612ac294061fbf3a04
Earl0fPudding <martin@weidenauer.cc>
2020-02-26 22:14:05 +0100

    Add german text for nav and footer

--- TypeWriter/settings.py
+++ TypeWriter/settings.py
@@ -13,13 +13,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 from django.utils.translation import ugettext_lazy as _
 
-IMPRINT = _('# Imprint \n' \
-            '' \
-            'imprint\n')
-PRIVACY_POLICY = _('# Privacy Policy \n' \
-                   '' \
-                   'pp')
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
