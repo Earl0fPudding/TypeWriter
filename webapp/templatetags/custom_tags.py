7c55a7ceaf14ea2fb44859136862be0edc852c94
Earl0fPudding <martin@weidenauer.cc>
2020-04-03 17:32:37 +0200

    Fix count non-public entries in index aside bar

--- webapp/templatetags/custom_tags.py
+++ webapp/templatetags/custom_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from webapp.models import Category, TranslatedSmalltext
+from webapp.models import Category, TranslatedSmalltext, Content
 
 register = template.Library()
 
@@ -10,3 +10,11 @@ def get_category_name(category_id, lang_short):
         translatable_smalltext_id=Category.objects.get(id=category_id).name_id,
         language__name_short__exact=lang_short
     ).text
+
+
+@register.filter
+def count_contents_by_categories(category_id, lang_short):
+    return len(Content.objects.filter(
+        entry__categories=Category.objects.get(id=category_id),
+        language__name_short__exact=lang_short,
+        is_public=True))
